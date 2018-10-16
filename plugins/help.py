6d90b3849acc539fcbb149160b49b41f418c05e8
Sergey Bakaleynik <bilaserg@gmail.com>
2018-10-16 20:37:42 +0300

    upd.

--- plugins/help.py
+++ plugins/help.py
@@ -12,6 +12,6 @@ async def help_me(message, attachments, env):
     user = user.response[0]
     text = 'Привет, {}!Я чат-бот для студентов. \
                 Могу показать расписание на любой день \
-                недели или неделю. расписание на <dd>.<mm>.<yyyy> - \
+                недели или неделю. \nрасписание на <dd>.<mm>.<yyyy> - \
                 расписание на конкретное число'.format(user['first_name'])
     await env.reply(text, keyboard=json.dumps(settings.keyboard, ensure_ascii=False))
