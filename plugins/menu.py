c361bfd6ec4635baa912a6413fafd303a1a81d02
Sergey Bakaleynik <bilaserg@gmail.com>
2018-10-23 21:51:02 +0300

    upd. menu

--- plugins/menu.py
+++ plugins/menu.py
@@ -1,11 +1,14 @@
 import json
 
 from kutana import Plugin
-import keyboards
+import keyboards, settings
 
 plugin = Plugin(name="меню")
 
 
 @plugin.on_text("меню", "menu", "назад", "back")
 async def help_me(message, attachments, env):
-    await env.reply('Меню', keyboard=json.dumps(keyboards.keyboard_main, ensure_ascii=False))
+    if message.from_id in settings.admins:
+        await env.reply('Меню', keyboard=json.dumps(keyboards.keyboard_main_admin, ensure_ascii=False))
+    else:
+        await env.reply('Меню', keyboard=json.dumps(keyboards.keyboard_main, ensure_ascii=False))
