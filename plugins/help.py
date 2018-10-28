57cebfa2160042f87796ca2a1ec55d8cb8163755
Sergey Bakaleynik <bilaserg@gmail.com>
2018-10-29 01:33:52 +0300

    help upd.

--- plugins/help.py
+++ plugins/help.py
@@ -15,7 +15,6 @@ async def help_me(message, attachments, env):
            'недели или неделю. \nрасписание на <dd>.<mm>.<yyyy> - ' \
            'расписание на конкретное число' \
         .format(user['first_name'])
-    print(message.from_id, type(message.from_id))
     if message.from_id in settings.admins:
         await env.reply(text, keyboard=json.dumps(keyboards.keyboard_main_admin, ensure_ascii=False))
     else:
